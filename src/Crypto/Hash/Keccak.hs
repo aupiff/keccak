2834613936d76eb85f9b2084db39fbadc4400912
Roy Blankman <riblankman@gmail.com>
2018-07-16 16:35:29 -0400

    updating docs & absorb func

--- src/Crypto/Hash/Keccak.hs
+++ src/Crypto/Hash/Keccak.hs
@@ -12,8 +12,8 @@ module Crypto.Hash.Keccak
       -- * Building blocks of a keccak sponge hash function
     , keccakHash
     , paddingKeccak
+    , paddingsha3
     , absorb
-    , toBlocks
     , squeeze
     ) where
 
@@ -27,9 +27,9 @@ type State = [[Word64]]
 emptyState :: State
 emptyState = replicate 5 (replicate 5 0)
 
---------------------------------------------------------
+----------------------------------------------------
 -- Constants used in KeccakF[1600] permutation
---------------------------------------------------------
+----------------------------------------------------
 
 roundConstants :: [Word64]
 roundConstants = [ 0x0000000000000001, 0x0000000000008082, 0x800000000000808A
@@ -49,14 +49,12 @@ rotationConstants = [ [  0, 36,  3, 41, 18 ]
                     , [ 27, 20, 39,  8, 14 ]
                     ]
 
-
---------------------------------------------------------
+----------------------------------------------------
 -- Keccak and SHA3 hash functions
---------------------------------------------------------
+----------------------------------------------------
 
 keccakHash :: Int -> BS.ByteString -> BS.ByteString
 keccakHash rate = squeeze outputBytes . absorb rate
-                                      . toBlocks bitrateBytes
                                       . paddingKeccak bitrateBytes
     where bitrateBytes = div rate 8
           outputBytes = div (1600 - rate) 16
@@ -64,47 +62,52 @@ keccakHash rate = squeeze outputBytes . absorb rate
 
 sha3Hash :: Int -> BS.ByteString -> BS.ByteString
 sha3Hash rate = squeeze outputBytes . absorb rate
-                                      . toBlocks bitrateBytes
-                                      . paddingSha3 bitrateBytes
+                                    . paddingSha3 bitrateBytes
     where bitrateBytes = div rate 8
           outputBytes = div (1600 - rate) 16
 
-
+-- | Keccak (512 bits) cryptographic hash algorithm
 keccak512 :: BS.ByteString -> BS.ByteString
 keccak512 = keccakHash 576
 
 
+-- | Keccak (384 bits) cryptographic hash algorithm
 keccak384 :: BS.ByteString -> BS.ByteString
 keccak384 =  keccakHash 832
 
 
+-- | Keccak (256 bits) cryptographic hash algorithm
 keccak256 :: BS.ByteString -> BS.ByteString
 keccak256 = keccakHash 1088
 
 
+-- | Keccak (224 bits) cryptographic hash algorithm
 keccak224 :: BS.ByteString -> BS.ByteString
 keccak224 = keccakHash 1152
 
 
+-- | SHA3 (512 bits) cryptographic hash algorithm
 sha3_512 :: BS.ByteString -> BS.ByteString
 sha3_512 = sha3Hash 576
 
 
+-- | SHA3 (384 bits) cryptographic hash algorithm
 sha3_384 :: BS.ByteString -> BS.ByteString
 sha3_384 = sha3Hash 832
 
 
+-- | SHA3 (256 bits) cryptographic hash algorithm
 sha3_256 :: BS.ByteString -> BS.ByteString
 sha3_256 = sha3Hash 1088
 
 
+-- | SHA3 (224 bits) cryptographic hash algorithm
 sha3_224 :: BS.ByteString -> BS.ByteString
 sha3_224 = sha3Hash 1152
 
-
---------------------------------------------------------
+----------------------------------------------------
 -- Padding functions
---------------------------------------------------------
+----------------------------------------------------
 
 -- | Multi-rate padding appends at least 2 bits and at most the number of bits
 -- in a block plus one.
@@ -115,13 +118,17 @@ multiratePadding bitrateBytes padByte input = BS.unpack . BS.append input $ if p
     where padlen = bitrateBytes - mod (BS.length input) bitrateBytes
 
 
+-- | Appends a single bit 1 followed by the minimum number of bits
+-- 0 followed by a single bit 1 such that the length of the result is
+-- a multiple of the bitrate.
 paddingKeccak :: Int -> BS.ByteString -> [Word8]
 paddingKeccak bitrateBytes = multiratePadding bitrateBytes 0x1
 
 
+-- | Appends to a message M padding of the form (M || 0x06 || 0x00... || 0x80)
+-- such that the length of the result is a multiple of the bitrate.
 paddingSha3 :: Int -> BS.ByteString -> [Word8]
 paddingSha3 bitrateBytes = multiratePadding bitrateBytes 0x6
-
 
 ----------------------------------------------------
 -- Sponge function primitives
@@ -138,12 +145,9 @@ toBlocks sizeInBytes input = let (a, b) = splitAt sizeInBytes input
           toLane :: [Word8] -> Word64
           toLane octets = foldl1 xor $ zipWith (\offset octet -> shiftL (fromIntegral octet) (offset * 8)) [0..7] octets
 
--- | Absorbtion phase
--- for each block Pi in P
---   S[x,y] = S[x,y] xor Pi[x+5*y],          for (x,y) such that x+5*y < r/w
---   S = Keccak-f[r+c](S)
-absorb :: Int -> [[Word64]] -> State
-absorb rate = foldl (absorbBlock rate) emptyState
+-- | Takes as input the bitrate @rate@ and a string P with |P| a multiple of @rate@. Returns the value of the state obtained after absorbing P.
+absorb :: Int -> [Word8] -> State
+absorb rate = foldl (absorbBlock rate) emptyState . toBlocks (div rate 8)
 
 absorbBlock :: Int -> State -> [Word64] -> State
 absorbBlock rate state input = keccakF state'
@@ -155,12 +159,7 @@ absorbBlock rate state input = keccakF state'
                             | x <- [0..4] ]
 
 
---  | Squeezing phase
---  Z = empty string
---  while output is requested
---    Z = Z || S[x,y],                  for (x,y) such that x+5*y < r/w
---    S = Keccak-f[r+c](S)
---    TODO handle longer outputs
+-- | Squeezing phase
 squeeze :: Int -> State -> BS.ByteString
 squeeze len = BS.pack . take len . stateToBytes
 
@@ -172,20 +171,15 @@ stateToBytes state = concat [ laneToBytes (state !! x !!  y) | y <- [0..4] , x <
 laneToBytes :: Word64 -> [Word8]
 laneToBytes word = fmap (\x -> fromIntegral (shiftR word (x * 8) .&. 0xFF)) [0..7]
 
-
--------------------------------------------------------------
+----------------------------------------------------
 -- KeccakF permutation & constituent primatives
--------------------------------------------------------------
-
+----------------------------------------------------
 
 keccakF :: State -> State
 keccakF state = foldl (\s r -> iota r . chi . rhoPi $ theta s) state [0 .. (rounds - 1)]
     where rounds = 24
 
 -- | θ step
--- C[x] = A[x,0] xor A[x,1] xor A[x,2] xor A[x,3] xor A[x,4],   for x in 0…4
--- D[x] = C[x-1] xor rot(C[x+1],1),                             for x in 0…4
--- A[x,y] = A[x,y] xor D[x],                           for (x,y) in (0…4,0…4)
 theta :: State -> State
 theta state = [ [ ((state !! x) !! y) `xor` (d !! x)
                     | y <- [0..4] ]
@@ -197,14 +191,12 @@ theta state = [ [ ((state !! x) !! y) `xor` (d !! x)
 
 
 -- | ρ and π steps
--- B[y,2*x+3*y] = rot(A[x,y], r[x,y]),        for (x,y) in (0…4,0…4)
 rhoPi :: State -> [[Word64]]
 rhoPi state = fmap (fmap rotFunc) [ [ ((x + 3 * y) `mod` 5, x) | y <- [0..4] ] | x <- [0..4] ]
     where rotFunc (x, y) = rotateL ((state !! x) !! y) ((rotationConstants !! x) !! y)
 
 
 -- | χ step
--- A[x,y] = B[x,y] xor ((not B[x+1,y]) and B[x+2,y]),  for (x,y) in (0…4,0…4)
 chi :: [[Word64]] -> State
 chi b = [ [ ((b !! x) !! y) `xor` (complement ((b !! ((x + 1) `mod` 5)) !! y) .&. ((b !! ((x + 2) `mod` 5)) !! y))
                     | y <- [0..4] ]
@@ -212,6 +204,5 @@ chi b = [ [ ((b !! x) !! y) `xor` (complement ((b !! ((x + 1) `mod` 5)) !! y) .&
 
 
 -- | ι step
--- A[0,0] = A[0,0] xor RC
 iota :: Int -> State -> State
 iota round ((first : rest) : restRows) = (xor (roundConstants !! round) first : rest) : restRows
